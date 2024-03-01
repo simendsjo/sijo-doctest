039516e828b5737569350a13ffc90c2d8346ad8c
Simen Endsjø <simendsjo@gmail.com>
2024-03-01 12:40:08 +0100

    Various tweaks
    - Tweaked to evaluate expected results to avoid problems with fset and readtables; #{||} cannot be used, and (fset:empty-map) neither
    - Improved working with multiple values
    - Added a test-package function to test all functions in a package
    - Only print when tests are failing (useful when testing entire packages)
    - RUN-DOCTESTS doesn't throw on empty docstring (triggered eof error)

--- doctest.lisp
+++ doctest.lisp
@@ -1,5 +1,6 @@
 ;;; Doctests for Common Lisp.
 ;;; Copyright (C) 2009 - 2013 Johan Lindberg, Pulp Software
+;;; Copyright (C) 2023 - 2024 Simen Endsjø <simen.endsjo@gmail.com>
 
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -14,13 +15,14 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(defpackage :doctest
-  (:use :common-lisp)
-  (:export :test
-	   :test-file
-	   :test-macro
-	   :test-function))
-(in-package :doctest)
+(defpackage :sijo-doctest
+  (:use #:cl)
+  (:export #:test-function
+           #:test-macro
+           #:test-file
+           #:test-package))
+
+(in-package :sijo-doctest)
 
 (defun whitespace-p (c)
   "Returns T if <c> is a whitespace character, otherwise NIL."
@@ -35,50 +37,51 @@
   (if (stringp string)
       (remove-if #'whitespace-p (copy-seq string))
       (remove-if #'whitespace-p (copy-seq (string string)))))
-    
+
 (defun string-equal-ignore-ws (string1 string2)
   (string-equal (remove-ws string1) (remove-ws string2)))
 
 (defun run-doctest (test-form expected-result expected-output output count)
   (let* ((test-form-signaled-condition 'NIL)
-	 (actual-output (make-array '(0) :element-type 'base-char
-				         :fill-pointer 0
-					 :adjustable t))
-	 (actual-result (multiple-value-list
-			 (handler-case
-			     (with-output-to-string
-				 (*standard-output* actual-output)
-			       (eval test-form))
-			   (error (co)
-			     (progn
-			       (setf test-form-signaled-condition t)
-			       co)))))
-	 (expected-output-matches-actual-output
-	  (if expected-output
-	      (string-equal-ignore-ws actual-output expected-output)
-	      T))
-	 (result T))
+         (actual-output (make-array '(0) :element-type 'base-char
+                                         :fill-pointer 0
+                                         :adjustable t))
+         (actual-result (multiple-value-list
+                         (handler-case
+                             (with-output-to-string
+                                 (*standard-output* actual-output)
+                               (eval test-form))
+                           (error (co)
+                             (progn
+                               (setf test-form-signaled-condition t)
+                               co)))))
+         (expected-result (multiple-value-list (eval expected-result)))
+         (expected-output-matches-actual-output
+           (if expected-output
+               (string-equal-ignore-ws actual-output expected-output)
+               T))
+         (result T))
     (if test-form-signaled-condition
         (when (not (equalp (type-of (car actual-result))
-			   (car expected-result)))
+                           (car expected-result)))
           (unless (subtypep (type-of (car actual-result)) 'warning)
             (setf result 'NIL)
             (format output "~&[~A] ~A signaled a ~A: ~A, expected ~A.~%"
-		           count test-form
-			   (type-of (car actual-result)) (car actual-result)
-			   (car expected-result))))
-	(unless (and (equalp actual-result expected-result)
-		     expected-output-matches-actual-output)
-	  (setf result 'NIL)
-	  (if expected-output-matches-actual-output
-	      (format output "~&[~A] ~A returned~{ ~A~}, expected~{ ~A~}.~%"
-		             count test-form
-			     actual-result
-			     expected-result)
-	      (format output "~&[~A] ~A printed \"~A\", expected \"~A\".~%"
-		             count test-form
-			     actual-output
-			     expected-output))))
+                    count test-form
+                    (type-of (car actual-result)) (car actual-result)
+                    (car expected-result))))
+        (unless (and (equalp actual-result expected-result)
+                     expected-output-matches-actual-output)
+          (setf result 'NIL)
+          (if expected-output-matches-actual-output
+              (format output "~&[~A] ~A returned~{ ~A~}, expected~{ ~A~}.~%"
+                      count test-form
+                      actual-result
+                      expected-result)
+              (format output "~&[~A] ~A printed \"~A\", expected \"~A\".~%"
+                      count test-form
+                      actual-output
+                      expected-output))))
     result))
 
 (defun run-doctests (docstring output)
@@ -86,33 +89,34 @@
    It returns the number of tests failed and passed and prints to <output>."
 
   (let ((tests-failed 0)
-	(tests-passed 0)
+        (tests-passed 0)
         (count 0))
-    (when docstring
+    (when (and docstring (not (eq :eof (peek-char nil docstring nil :eof))))
       (do ((c (read-char docstring)
-	      (read-char docstring nil 'EOF)))
-	  ((eq c 'EOF))
-	(when (and (equal #\> c)
-		   (equal #\> (read-char docstring))
-		   (whitespace-p (peek-char nil docstring)))
-	  (let ((test-form (read docstring))
-		(expected-result (list (read docstring)))
-		(expected-output 'NIL))
-	    (when (and (symbolp (car expected-result))
-		       (equal (string (car expected-result)) "->"))
-	      (setf expected-output (read docstring))
-	      (setf expected-result (list (read docstring))))
-	    
-	    (if (run-doctest test-form
-			     expected-result
-			     expected-output
-			     output
-			     (incf count))
-		(incf tests-passed)
-		(incf tests-failed))))))
+              (read-char docstring nil 'EOF)))
+          ((eq c 'EOF))
+        (when (and (equal #\> c)
+                   (equal #\> (read-char docstring))
+                   (whitespace-p (peek-char nil docstring)))
+          (let ((test-form (read docstring))
+                (expected-result (list (read docstring)))
+                (expected-output 'NIL))
+            (when (and (symbolp (car expected-result))
+                       (equal (string (car expected-result)) "->"))
+              (setf expected-output (read docstring))
+              (setf expected-result (list (read docstring))))
+
+            (if (run-doctest test-form
+                             (car expected-result)
+                             expected-output
+                             output
+                             (incf count))
+                (incf tests-passed)
+                (incf tests-failed))))))
 
     (values tests-failed tests-passed)))
 
+#+(or)
 (defun test (thing &key (output t))
   "Test extracts and tests code snippets embedded in the documentation string
    of <thing>. It returns the number of tests failed and passed and prints a
@@ -128,30 +132,30 @@
    >> 1 ; NOTE! You can use comments to clarify!
    1
 
-   If you expect more than one value you should wrap it in a multiple-value-
-   list to create one form.
+   If you expect more than one value you should use the values function in the
+   expected results.
 
-   >> (multiple-value-list (values 1 2))
-   (1 2)
+   >> (values 1 2)
+   (values 1 2)
 
    Newlines and other whitespace (including comments) doesn't particularly
-   matter. We could just as well have written >> (multiple-value-list (values 1
-   2)) (1 2) instead.
+   matter. We could just as well have written >> (values 1
+   2) (values 1 2) instead.
 
    If you test a thing that doesn't have a documentation string, test will
    return NIL.
 
    >> (defun sqr (x)
         (* x x))
-   SQR
-   >> (doctest:test #'sqr)
+   'SQR
+   >> (sijo-doctest::test #'sqr)
    NIL
 
    If you need to test that a function signals a condition for certain inputs
    you can use the name of the condition as the expected return value.
 
    >> (sqr 'x)
-   TYPE-ERROR
+   'TYPE-ERROR
 
    If we add a documentation string for sqr with a doctest, we can verify that
    tests can fail as well.
@@ -162,12 +166,12 @@
           This test will fail:
           >> (sqr 3) 3\"
         (* x x))
-   SQR
+   'SQR
 
    Testing sqr with test should now return 1 failed and 0 passed.
 
-   >> (multiple-value-list (doctest:test #'sqr))
-   (1 0)
+   >> (sijo-doctest::test #'sqr)
+   (values 1 0)
 
    If you need to test the output of a function you can add an expected output
    form (written as -> <expected-output>) *between* the function call and the
@@ -203,7 +207,7 @@
           -> |Blah blah blah|
           NIL\"
         (format t \"~A * ~A = ~A\" x x (* x x)))
-   SQR
+   'SQR
 
    Testing sqr with test should now return 1 failed and 2 passed. It should
    also inform us that:
@@ -213,21 +217,21 @@
 
    NOTE! Whitespace is ignored when output is compared.
 
-   >> (multiple-value-list (doctest:test #'sqr :output T))
+   >> (sijo-doctest::test #'sqr :output T)
    -> |[4] (SQR 2) printed \"2 * 2 = 4\", expected \"Blah blah blah\".
        Results for SQR (FUNCTION): 1 of 4 failed.|
-   (1 3)"
+   (values 1 3)"
 
   (cond ((functionp thing)
-	 (test-function thing :output output))
-	((pathnamep thing)
-	 (test-file thing :output output))
-	((and (symbolp thing)
-	      (macro-function thing))
-	 (test-macro thing :output output))
-	
-	(t
-	 (error "~&No suitable testing-function available for ~A~%" thing))))
+         (test-function thing :output output))
+        ((pathnamep thing)
+         (test-file thing :output output))
+        ((and (symbolp thing)
+              (macro-function thing))
+         (test-macro thing :output output))
+
+        (t
+         (error "~&No suitable testing-function available for ~A~%" thing))))
 
 (defun test-function (function &key (output t))
   "Test-function extracts and tests code snippets in <function>'s documentation
@@ -239,9 +243,9 @@
   (when (documentation function 'function)
     (let ((function-name (third (multiple-value-list (function-lambda-expression function)))))
       (multiple-value-bind (tests-failed tests-passed)
-	  (with-input-from-string (docstring (documentation function 'function))
-	    (run-doctests docstring output))
-	(print-results function-name 'function output tests-failed tests-passed)))))
+          (with-input-from-string (docstring (documentation function 'function))
+            (run-doctests docstring output))
+        (print-results function-name 'function output tests-failed tests-passed)))))
 
 (defun test-macro (macro &key (output t))
   "Test-macro extracts and tests code snippets in <macro>'s documentation string.
@@ -253,9 +257,9 @@
   (when (documentation macro 'function)
     (let ((macro-name (third (multiple-value-list (function-lambda-expression (macro-function macro))))))
       (multiple-value-bind (tests-failed tests-passed)
-	  (with-input-from-string (docstring (documentation macro 'function))
-	    (run-doctests docstring output))
-	(print-results macro-name 'macro output tests-failed tests-passed)))))
+          (with-input-from-string (docstring (documentation macro 'function))
+            (run-doctests docstring output))
+        (print-results macro-name 'macro output tests-failed tests-passed)))))
 
 
 (defun test-file (filename &key (output t))
@@ -265,13 +269,20 @@
 
    See also the documentation string for test."
 
-    (multiple-value-bind (tests-failed tests-passed)
-        (with-open-file (docstring filename :direction :input)
-          (run-doctests docstring output))
-      (print-results filename 'file output tests-failed tests-passed)))
+  (multiple-value-bind (tests-failed tests-passed)
+      (with-open-file (docstring filename :direction :input)
+        (run-doctests docstring output))
+    (print-results filename 'file output tests-failed tests-passed)))
+
+(defun test-package (package)
+  (let ((*package* (find-package package)))
+    (do-symbols (symbol (find-package package))
+      (when (fboundp symbol)
+        (test-function (symbol-function symbol))))))
 
 (defun print-results (test-name test-type output tests-failed tests-passed)
-  (format output "~&Results for ~A (~A): ~D of ~D failed.~%"
-	         test-name test-type
-		 tests-failed (+ tests-failed tests-passed))
+  (when (> tests-failed 0)
+    (format output "~&Results for ~A (~A): ~D of ~D failed.~%"
+            test-name test-type
+            tests-failed (+ tests-failed tests-passed)))
   (values tests-failed tests-passed))
